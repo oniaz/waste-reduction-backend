eb6e847225cc174bc1b487d696b6439ce796f8cb
Bassant <basantsafty@gamil.com>
2026-06-03 23:30:25 +0300

    feat: create adminLogs model with schema

---
+++ src/models/adminLogs.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const adminLogsSchema = new mongoose.Schema({
+    adminId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Admin', // Targets your exported Admin model name
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'UsersAuth', // Targets your exported UsersAuth model name
+        // Not required since some actions may not target a specific user (e.g., system-wide actions)
+    },
+    action: {
+        type: String,
+        required: true,
+        enum: ["suspend_user", "activate_user", "approve_vendor", "reject_vendor"] // Expand as needed
+    },
+    description: {
+        type: String,
+    }
+}, { timestamps: true });
+
+const AdminLogs = mongoose.model('AdminLogs', adminLogsSchema);
+export default AdminLogs;
