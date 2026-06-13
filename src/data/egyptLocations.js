bc825b3a1d86eb50c511eee192da040af8711cd8
oniaz <32391049+oniaz@users.noreply.github.com>
2026-06-14 01:37:50 +0300

    feat: add location data for Cairo, Alexandria, and Giza

---
+++ src/data/egyptLocations.js
@@ -0,0 +1,163 @@
+// Location data for Cairo, Alexandria, and Giza.
+
+const EGYPT_LOCATIONS = [
+  {
+    governorateId: "cairo",
+    governorateName: "Cairo",
+    cities: [
+      {
+        cityId: "cairo_city",
+        cityName: "Cairo City",
+        neighborhoods: [
+          "Maadi",
+          "Zamalek",
+          "Nasr City",
+          "Heliopolis",
+          "Downtown Cairo",
+          "New Cairo / Fifth Settlement",
+          "Shoubra",
+          "Abbaseya",
+          "Ain Shams",
+          "Al Amiriya",
+          "Al Basatin",
+          "Al Darb al-Ahmar",
+          "Al Faggala",
+          "Al Matareya",
+          "Al Muski",
+          "Al Rehab City",
+          "Al Sayeda Zeinab",
+          "Al Shorouk City",
+          "Al Wailey",
+          "Al Zaher",
+          "Al Zaytoun",
+          "Bab al-Shariya",
+          "Badr City",
+          "Boulaq",
+          "El Marg",
+          "El Salam City",
+          "Garden City",
+          "Helwan",
+          "Madinaty",
+          "Mokattam",
+          "Old Cairo / Masr El Qadima",
+          "Sheraton",
+          "Tagamoa"
+        ]
+      }
+    ]
+  },
+  {
+    governorateId: "alexandria",
+    governorateName: "Alexandria",
+    cities: [
+      {
+        cityId: "alexandria_city",
+        cityName: "Alexandria City",
+        neighborhoods: [
+          "Smouha",
+          "Sidi Bishr",
+          "Glim",
+          "San Stefano",
+          "Al Attarin",
+          "Al Manshiyya",
+          "Al Wardian",
+          "Amreya",
+          "Asafra",
+          "Azarita",
+          "Bacos",
+          "Bolkly",
+          "Camp Caesar",
+          "Cleopatra",
+          "El Ibrahimiyya",
+          "El Agami",
+          "El Mandara",
+          "El Mex",
+          "Fleming",
+          "Janaklis",
+          "Kafr Abdo",
+          "Loran",
+          "Miami",
+          "Moharam Bek",
+          "Mostafa Kamel",
+          "Rusha",
+          "Saba Pasha",
+          "Sidi Gaber",
+          "Sporting",
+          "Stanley",
+          "Tharwat",
+          "Victoria",
+          "Zizinia"
+        ]
+      },
+      {
+        cityId: "borg_el_arab",
+        cityName: "Borg El Arab",
+        neighborhoods: [
+          "New Borg El Arab City",
+          "Industrial Zone",
+          "Borg El Arab Village"
+        ]
+      }
+    ]
+  },
+  {
+    governorateId: "giza",
+    governorateName: "Giza",
+    cities: [
+      {
+        cityId: "giza_city",
+        cityName: "Giza City",
+        neighborhoods: [
+          "Dokki",
+          "Mohandessin",
+          "Agouza",
+          "Faisal",
+          "Haram",
+          "Imbaba",
+          "Kit Kat",
+          "Mounira",
+          "Omraneya",
+          "Sakiat Mekky",
+          "Boulaq El Dakrour",
+          "Meneil"
+        ]
+      },
+      {
+        cityId: "october_city",
+        cityName: "6th of October City",
+        neighborhoods: [
+          "1st District",
+          "2nd District",
+          "3rd District",
+          "4th District",
+          "5th District",
+          "6th District",
+          "7th District",
+          "8th District",
+          "9th District",
+          "10th District",
+          "11th District",
+          "12th District",
+          "Sheikh Zayed City",
+          "October Gardens",
+          "Industrial Zone",
+          "Hadayek Al Ahram"
+        ]
+      },
+      {
+        cityId: "giza_suburbs",
+        cityName: "Giza Suburbs / Centers",
+        neighborhoods: [
+          "Kerdasa",
+          "Al Badrashein",
+          "Al Ayat",
+          "Al Hawamdeya",
+          "Osim",
+          "Abu Nomros"
+        ]
+      }
+    ]
+  }
+];
+
+export default EGYPT_LOCATIONS;
